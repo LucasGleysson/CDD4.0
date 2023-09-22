742c805d5103f64ec0463363f3523e96cd14aab9
lucasgleysson <dlucasgleysson@gmail.com>
2023-09-22 17:24:18 -0300

    Array e for

--- exercicio33.py
+++ exercicio33.py
@@ -1,8 +1,7 @@
 a = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 m = a
 for i in range(10):
-    num = int(input(f'Digite o {i+1}º valor: '))
-    a[i] = num
+    a[i] = int(input(f'Digite o {i+1}º valor: '))
 
 digito = int(input('Digite o valor que vai mutiplicar os números: '))
 for j in range(10):
