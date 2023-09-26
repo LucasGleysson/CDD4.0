e44268fc6f46cff0db51d723b4277f11283d1dbd
lucasgleysson <dlucasgleysson@gmail.com>
2023-09-26 14:18:44 -0300

    Revisão exercicio01

--- revisao/Exercico01.py
+++ revisao/Exercico01.py
@@ -2,4 +2,11 @@ nota1 = float(input('Digite a primeira nota: '))
 nota2 = float(input('Digite a segunda nota: '))
 
 media = (nota1 + nota2) / 2
+situacao = "APROVADO"
+
+if media < 7:
+    situacao = "RECUPERACAO"
+elif media < 4:
+    situacao = "REPROVADO"
+
 print(f'A média é {media:.1f}')
