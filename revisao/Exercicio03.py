246bdd7610a26d39e0e740d1c44ac442c493d7a6
lucasgleysson <dlucasgleysson@gmail.com>
2023-09-26 16:22:44 -0300

    revisao EX03

--- revisao/Exercicio03.py
+++ revisao/Exercicio03.py
@@ -1,10 +1,14 @@
-ano_atual = 2023
-mes_atual = int(input('Digite o número do mês atual: '))
-mes_nascimento = int(input('Digite o mês em que você nasceu: '))
-idade = int(input('Digite a idade: '))
+finalizar = 'n'
+while finalizar != 's':
+    ano_atual = 2023
+    mes_atual = int(input('Digite o número do mês atual: '))
+    mes_nascimento = int(input('Digite o mês em que você nasceu: '))
+    idade = int(input('Digite a idade: '))
 
-if mes_atual < mes_nascimento:
-    ano_atual -= 1
+    if mes_atual < mes_nascimento:
+        ano_atual -= 1
 
-ano_nascimento = ano_atual - idade
-print(f'A pessoa nasceu no ano de {ano_nascimento}')
+    ano_nascimento = ano_atual - idade
+    print(f'A pessoa nasceu no ano de {ano_nascimento}')
+
+    finalizar = input('Deseja finalizar? (s/n): ')
