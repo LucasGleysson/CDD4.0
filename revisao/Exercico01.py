bf1d8727405b0242b0669672132c92ee880ffb17
lucasgleysson <dlucasgleysson@gmail.com>
2023-09-26 14:44:05 -0300

    revisão

--- revisao/Exercico01.py
+++ revisao/Exercico01.py
@@ -1,12 +1,17 @@
-nota1 = float(input('Digite a primeira nota: '))
-nota2 = float(input('Digite a segunda nota: '))
+repete = 's'
+while repete != 'n':
+    nota1 = float(input('Digite a primeira nota: '))
+    nota2 = float(input('Digite a segunda nota: '))
 
-media = (nota1 + nota2) / 2
-situacao = "APROVADO"
+    media = (nota1 + nota2) / 2
 
-if media < 7:
-    situacao = "RECUPERACAO"
-elif media < 4:
-    situacao = "REPROVADO"
+    situacao = "APROVADO"
+    if media < 4:
+        situacao = "REPROVADO"
+    elif media < 7:
+        situacao = "RECUPERAÇÃO"
 
-print(f'A média é {media:.1f}')
+    print(f'A média é {media:.1f}')
+    print(f'O aluno está {situacao}')
+
+    repete = input('Você quer repetir? (s / n): ')
