c8e10c8adbe70e9c78db07edc8e2e268f6aed65d
lucasgleysson <dlucasgleysson@gmail.com>
2023-09-26 15:13:25 -0300

    revisão EX02

--- revisao/Exercicio02.py
+++ revisao/Exercicio02.py
@@ -1,12 +1,14 @@
-num = int(input('Digite um número diferente de zero: '))
-
-while num == 0:
-    print('Valor invalido, tente novamente')
-
+finalizar = 'n'
+while finalizar != 's':
     num = int(input('Digite um número diferente de zero: '))
 
-if num > 0:
-    print(f'{num} é POSITIVO')
-else:
-    print(f'{num} é NEGATIVO')
+    if num == 0:
+        print('Valor invalido, tente novamente')
+        continue
+    if num > 0:
+        print(f'{num} é POSITIVO')
+    else:
+        print(f'{num} é NEGATIVO')
+
+    finalizar = input('Finalizar? (s/n): ')
 
