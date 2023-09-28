c510a6724388a561694a9bcfdf99e168675467ab
lucasgleysson <dlucasgleysson@gmail.com>
2023-09-28 14:42:30 -0300

    revisao EX08

--- revisao/Exercicio08.py
+++ revisao/Exercicio08.py
@@ -4,6 +4,6 @@ for i in range(qnt):
     num = float(input(f'Digite o {qnt+1}º número: '))
     soma += num
 
-media = soma / qnt+1
+media = soma / qnt
 print(f'A soma dos números digitados é {soma}')
 print(f'A média dos números é: {media}')
