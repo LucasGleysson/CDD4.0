4fc28107894b5b5d0f1ce6d29b3db599b60cd556
lucasgleysson <dlucasgleysson@gmail.com>
2023-09-28 14:38:16 -0300

    revisao EX08

---
+++ revisao/Exercicio08.py
@@ -0,0 +1,9 @@
+qnt = int(input('Digite a quantidade de números que serão calculados: '))
+soma = 0
+for i in range(qnt):
+    num = int(input(f'Digite o {qnt+1}º número: '))
+    soma += num
+
+media = soma / qnt+1
+print(f'A soma dos números digitados é {soma}')
+print(f'A média dos números é: {media}')
